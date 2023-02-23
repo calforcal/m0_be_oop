df8b45d2bd206786c88c1635e0442fa58822710a
Michael Callahan <mcalla242@gmail.com>
2023-02-23 16:51:19 -0700

    Add solutions for OOP CFU

--- oop.rb
+++ oop.rb
@@ -3,6 +3,20 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+    def initialize(name)
+        @name = name
+        @color = "silver"
+    end
+
+    def say(message)
+        puts "*~* #{message}*~*"
+    end
+end
+
+unicorn1 = Unicorn.new("Tabitha")
+p unicorn1
+unicorn1.say("Helllllllllo")
 
 
 #  Write a class called Vampire
@@ -10,6 +24,23 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+
+class Vampire
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def drink
+        @thirsty = false
+    end
+end
+
+vampire1 = Vampire.new("Dracula", "Pikachu")
+p vampire1
+vampire1.drink
+p vampire1
 
 
 #  Write a Dragon class
@@ -19,6 +50,30 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+        @num_feeds = 0
+    end
+    
+    def eat(feeds = 1)
+        @num_feeds += feeds
+
+        if @num_feeds >= 4
+            @is_hungry = false
+        end
+    end
+end
+
+dragon1 = Dragon.new("Blaze", "Victory", "Red")
+p dragon1
+dragon1.eat
+p dragon1
+dragon1.eat(3)
+p dragon1
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +83,35 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    def initialize(name, disposition, age = 0)
+        @name = name
+        @disposition = disposition
+        @age = age
+
+        @is_adult = false
+            if @age >= 33
+                @is_adult = true
+            end
+
+        @is_old = false
+            if @age >= 101
+                @is_old = true
+            end
+
+        @has_ring = false
+            if @name.downcase == "frodo"
+                @has_ring = true
+            end 
+    end
+
+    def celebrate_birthday
+        @age += 1
+    end
+end
+
+hobbit1 = Hobbit.new("Frodo", "Inspired", 40)
+p hobbit1
+hobbit1.celebrate_birthday
+p hobbit1
